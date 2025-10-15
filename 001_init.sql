8675b503fe5cd0af657572bfebc14972e22b8b26
Ferhat Hamza <Ferhathamza17@gmail.com>
2025-10-15 22:39:36 +0100

    Remove initial user data from 001_init.sql
    Removed initial user insert statements from the SQL file.

--- 001_init.sql
+++ 001_init.sql
@@ -16,15 +16,3 @@ CREATE TABLE vaccination (
   quantiteAdministree INTEGER
 );
 
--- Initial users
-INSERT INTO users (username, password, role, etab) VALUES
-('admin', 'G9v@3RtZ', 'admin', 'ADMIN'),
-('ehs_ghardaia', 'm8#Qp6Lw', 'coordinateur', 'EHS Ghardaia'),
-('epsp_ghardaia', '7Xr%2bNk', 'coordinateur', 'EPSP Ghardaia'),
-('epsp_metlili', 'hP4&z9Yq', 'coordinateur', 'EPSP Metlili'),
-('epsp_guerrara', '!T6sV2mB', 'coordinateur', 'EPSP Guerrara'),
-('epsp_berriane', 'R3#kH8uS', 'coordinateur', 'EPSP Berriane'),
-('eph_ghardaia', 'n5$Gq7Zj', 'coordinateur', 'EPH Ghardaia'),
-('eph_metlili', 'Yt9^4pLm', 'coordinateur', 'EPH Metlili'),
-('eph_guerrara', '2b@Vw6Qx', 'coordinateur', 'EPH Guerrara'),
-('eph_berriane', 'cK7*R2hZ', 'coordinateur', 'EPH Berriane');
